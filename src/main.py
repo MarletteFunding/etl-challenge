7865f8ea8ca7a534eca8d551bff2ffb5a3dde23d
Waqar Ahmed <waqar.ahmed@monsanto.com>
2021-04-02 22:20:01 -0500

    Appended by Waqar Ahmed

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 
 from database import Database
 
@@ -18,6 +19,13 @@ class EtlScript:
 
     def run(self):
         # Your code starts here
+        print("function run started")
+        df=pd.read_csv(self.data_file, sep="|", header=None)
+        columns_list = open(self.header_file).read().splitlines()
+        df.columns=columns_list
+        df.to_csv(self.out_file, mode="w", index=False)
+        EtlScript().load_file_to_database(self.out_file)
+        print("function run finished")        
         pass
 
 if __name__ == "__main__":
